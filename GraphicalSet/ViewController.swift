7cfcec13656a73def90ee5b8e7e093862cf33868
Peter Sevcik <psevcikbn@gmail.com>
2019-01-15 14:46:07 +0100

    Initial Commit

---
+++ GraphicalSet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GraphicalSet
+//
+//  Created by Peter on 15/01/2019.
+//  Copyright © 2019 Excellence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
